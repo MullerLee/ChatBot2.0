81c18678600c6d7b35cd3e22e018e4cf4b471cf3
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2017-10-27 12:53:45 +0800

    Update NLTK.py

--- NLTK.py
+++ NLTK.py
@@ -5,3 +5,10 @@ along with a suite of text processing libraries for classification, tokenization
 stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum."
 from nltk.tokenize import sent_tokenize
 sent_tokenize(text)
+
+
+
+
+french_tokenizer = nltk.data.load('tokenizers/punkt/french.pickle')
+text = "Sois toujours l'coute de tes plus profonds dsirs. Tiens eux comme tu tiens la vie, car sans eux, la vie n'est rien. "
+french_tokenizer.tokenize(text)
