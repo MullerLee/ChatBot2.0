567910ae6426bd94f5c670fd3872392d08262397
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2017-10-27 12:36:53 +0800

    Update NLTK.py

--- NLTK.py
+++ NLTK.py
@@ -1,1 +1,7 @@
 import nltk
+text = "NLTK is a leading platform for building Python programs to work with human language data. 
+It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, 
+along with a suite of text processing libraries for classification, tokenization, 
+stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum."
+from nltk.tokenize import sent_tokenize
+sent_tokenize(text)
