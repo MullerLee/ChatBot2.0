e17687ac487d0095af3553189345b60697412782
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2017-10-27 15:46:49 +0800

    Update NLTK.py

--- NLTK.py
+++ NLTK.py
@@ -1,3 +1,4 @@
+#This Parts aim to give an example on string dividing.
 import nltk
 text = "NLTK is a leading platform for building Python programs to work with human language data. 
 It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, 
@@ -9,3 +10,6 @@ sent_tokenize(text)
 french_tokenizer = nltk.data.load('tokenizers/punkt/french.pickle')
 text = "Sois toujours l'coute de tes plus profonds dsirs. Tiens eux comme tu tiens la vie, car sans eux, la vie n'est rien. "
 french_tokenizer.tokenize(text)
+
+text = nltk.word_tokenize("NLTK is a leading platform for building Python programs to work with human language data. It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, along with a suite of text processing libraries for classification, tokenization, stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum.")
+print(text)
