58b09253426a11aa5176877d0b858cf3fc2765c6
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2017-10-28 20:55:23 +0800

    Update RegularTokenize.py

--- RegularTokenize.py
+++ RegularTokenize.py
@@ -1,1 +1,14 @@
 import nltk
+from nltk.tokenize import RegexpTokenizer
+tokenizer=RegexpTokenizer("[\w]+")
+tokenizer.tokenize("Don't heisitate to ask questions.")
+
+import nltk
+from nltk.tokenize import regexp_tokenize
+sent="Don't heisitate to ask questions."
+print(regexp_tokenize(sent,pattern='\w+|\$[\d\.]+|\S+'))
+
+import nltk
+from nltk.tokenize import RegexpTokenizer
+tokenizer=RegexpTokenizer('\s+',gaps=True)
+tokenizer.tokenize("Don't heisitate to ask questions")
